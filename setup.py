72c1cfb004385a10375bb0505e8c1c245d50977c
hexatester <revolusi147id@gmail.com>
2020-12-01 22:05:35 +0700

    prepare installer

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+
+__version__ = ""
+packages = find_packages(exclude=["tests*", "template*"])
+
+fn = os.path.join("telegram", "version.py")
+with open(fn) as fh:
+    code = compile(fh.read(), fn, "exec")
+    exec(code)
+
+
+setup(
+    name="simdak",
+    version=__version__,
+    author="hexatester",
+    license="GPLv3",
+    url="https://github.com/dapodix/simdak",
+    keywords="dapodik simdak paud-dikmas kemdikbud",
+    description="Importer-exporter data Simdak Kemdikbud",
+    packages=packages,
+    package_data={"simdak": ["template/*.xlsx"]},
+    install_requires=["python", "requests", "bs4", "openpyxl", "lxml"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education :: Testing",
+        "Topic :: Education",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    entry_points={"console_scripts": ["simdak=simdak.__main__:main"]},
+)
--- simdak/__init__.py
+++ simdak/__init__.py
@@ -1,5 +1,5 @@
-__version__ = "0.1.0"
-
 from .paud import SimdakPaud
+from .template import TEMPLATE_DIR
+from .version import __version__
 
 __all__ = ["SimdakPaud"]
