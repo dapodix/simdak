b5e88833159cc6fee5f4625bc9506deffcf39707
hexatester <revolusi147id@gmail.com>
2020-12-01 22:06:48 +0700

    add click

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     description="Importer-exporter data Simdak Kemdikbud",
     packages=packages,
     package_data={"simdak": ["template/*.xlsx"]},
-    install_requires=["python", "requests", "bs4", "openpyxl", "lxml"],
+    install_requires=["python", "requests", "bs4", "openpyxl", "lxml", "click"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
