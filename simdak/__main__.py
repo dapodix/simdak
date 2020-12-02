78b3f170e9db0097648f1af8b1e8d464f9027b9a
hexatester <revolusi147id@gmail.com>
2020-12-02 17:57:52 +0700

    improve cli

--- simdak/__main__.py
+++ simdak/__main__.py
@@ -2,55 +2,61 @@ import click
 import os
 import shutil
 
-from . import paud
+from . import paud as simdak_paud
 from .template import TEMPLATE_FILE
 
 CWD = os.getcwd()
 
-
-class Setting(object):
-    def __init__(self, email: str, password: str, debug: bool = False):
-        self.email = email
-        self.password = password
-        self.debug = debug
+EMAIL_HELP = "Alamat email"
+PASSWORD_HELP = "Kata sandi / email"
+SHEET_HELP = "Nama Sheet excel yang digunakan dokumen"
 
 
-@click.group()
-@click.option("--email", prompt=True)
-@click.password_option()
-@click.option("--debug/--no-debug", default=False)
-@click.pass_context
-def main(ctx: click.Context, email: str, password: str, debug: bool):
-    ctx.obj = Setting(email, password)
+@click.group("paud")
+def paud():
+    pass
 
 
-@click.command("template")
-@click.argument("nama", default="Simdak-Paud.xlsx")
-def template(nama: str):
-    click.echo("Membuat template...")
-    shutil.copy(
-        TEMPLATE_FILE,
-        os.path.join(CWD, nama),
-    )
-
-
-@click.command("export")
-@click.pass_context
+@paud.command("template")  # type: ignore
 @click.argument("nama", default="Simdak-Paud.xlsx", required=True)
-def exports(ctx, nama: str):
-    setting: Setting = ctx.obj
-    filepath = os.path.join(CWD, nama)
-    paud.exports(filepath, setting.email, setting.password)
+def template(name: str):
+    name = name if name.endswith(".xlsx") else name + ".xlsx"
+    click.echo(f"Membuat template dengan nama {name}")
+    dst = os.path.abspath(os.path.join(CWD, name))
+    shutil.copy(TEMPLATE_FILE, dst)
 
 
-@click.command("import")
-@click.pass_context
-@click.argument("nama", default="Simdak-Paud.xlsx", required=True)
-def imports(ctx, nama: str):
-    setting: Setting = ctx.obj
-    filepath = os.path.join(CWD, nama)
-    paud.imports(filepath, setting.email, setting.password)
+@paud.command("export")  # type: ignore
+@click.option("--email", required=True, prompt=True, help=EMAIL_HELP)
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    confirmation_prompt=True,
+    help=PASSWORD_HELP,
+)
+@click.option("--sheet", default="Sheet1", required=False, sheet=SHEET_HELP)
+@click.argument("file", default="Simdak-Paud.xlsx", required=True)
+def exports(email: str, password: str, file: str):
+    simdak_paud.exports(email, password, file)
 
+
+@paud.command("import")  # type: ignore
+@click.option("--email", required=True, prompt=True, help=EMAIL_HELP)
+@click.option(
+    "--password",
+    prompt=True,
+    hide_input=True,
+    confirmation_prompt=True,
+    help=PASSWORD_HELP,
+)
+@click.option("--sheet", default="Sheet1", required=False, sheet=SHEET_HELP)
+@click.argument("file", default="Simdak-Paud.xlsx", required=True)
+def imports(email: str, password: str, sheet: str, file: str):
+    simdak_paud.exports(email, password, file)
+
+
+main = click.CommandCollection("simdak", sources=[paud])
 
 if __name__ == "__main__":
     main()
