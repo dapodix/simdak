7e5ad75cd2aa1ab59763ae6eb13847c88a07bfc8
hexatester <revolusi147id@gmail.com>
2020-12-25 21:46:57 +0700

    update UA

--- simdak/config.py
+++ simdak/config.py
@@ -1,4 +1,3 @@
-
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"
 }
