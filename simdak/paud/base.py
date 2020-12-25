632bb57b22ca4ac9d8034988991171900f63de80
hexatester <revolusi147id@gmail.com>
2020-12-25 21:46:11 +0700

    update base

--- simdak/paud/base.py
+++ simdak/paud/base.py
@@ -1,16 +1,29 @@
-from logging import getLogger
-from requests import Session
+from dataclasses import dataclass
+from logging import getLogger, Logger
+from requests import Session, get
+
+from simdak.config import HEADERS
 
 
 class BaseSimdakPaud(object):
-    _domain = "https://app.paud-dikmas.kemdikbud.go.id"
-    _base_url = "https://app.paud-dikmas.kemdikbud.go.id/simdak/"
-    _session = Session()
-    _login = False
-    _modul = False
-    _logger = getLogger("SimdakPaud")
+    _domain: str = "https://dapo.paud-dikmas.kemdikbud.go.id"
+    _base_url: str = "https://dapo.paud-dikmas.kemdikbud.go.id/simdak/"
+    _logger: Logger = getLogger("simdak-paud")
+    _session: Session = Session()
+
+    def __init__(
+        self,
+        login: bool = False,
+        modul: bool = False,
+        logger: Logger = getLogger("simdak-paud"),
+        retry: int = 0,
+    ):
+        self._login: bool = login
+        self._modul: bool = modul
+        self._logger: Logger = logger
+        self._retry: int = retry
+        self._session.headers.update(HEADERS)
 
     @classmethod
-    def is_online(cls) -> bool:
-        res = cls._session.get(cls._base_url + "site/login")
-        return res.ok
+    def is_online(cls, url: str = "site/login") -> bool:
+        return get(cls._base_url + url).ok
